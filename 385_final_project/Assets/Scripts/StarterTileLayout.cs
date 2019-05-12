ee1e82e84d767af1d0b5fe231c297b29393aae97
Petra Power <petra.powered@gmail.com>
2019-05-12 16:32:45 -0700

    starting to add water to the map

--- 385_final_project/Assets/Scripts/StarterTileLayout.cs
+++ 385_final_project/Assets/Scripts/StarterTileLayout.cs
@@ -6,24 +6,46 @@ public class StarterTileLayout : MonoBehaviour
 {
     public int mapSize = 10;
     public GameObject plainsTile;
+    public GameObject waterTile;
     public Camera gameCamera;
+
+    private GameObject[,] tileMap;
 
     // Start is called before the first frame update
     void Start()
     {
-        // start with plains
-        for(int i = 0; i < mapSize; i++)
-        { 
-            for(int j = 0; j < mapSize; j++)
-            {
-                Vector3 tilePosition = new Vector3(i * 0.86f, j * 0.86f, 0);
-                print(tilePosition);
-                Instantiate(plainsTile, tilePosition, Quaternion.identity);
-            }
-        }
+        // make map based on given size
+        tileMap = new GameObject[mapSize, mapSize];
+
+        GeneratePlains();
+        GenerateWater();
 
         // position the camera in the middle of the map
-        gameCamera.transform.position = new Vector3((mapSize/2) * 0.86f, (mapSize/2) * 0.86f, -20);
+        gameCamera.transform.position = new Vector3((mapSize / 2) * 0.86f, (mapSize / 2) * 0.86f, -20);
+    }
+
+    private void GeneratePlains()
+    {
+        for (int i = 0; i < mapSize; i++)
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                Vector3 plainTilePosition = new Vector3(i * 0.86f, j * 0.86f, 0);
+                tileMap[i, j] = Instantiate(plainsTile, plainTilePosition, Quaternion.identity);
+            }
+        }
+    }
+
+    private void GenerateWater()
+    {
+        System.Random rand = new System.Random();
+        int waterPositionX = rand.Next(mapSize);
+        rand = new System.Random();
+        int waterPositionY = rand.Next(mapSize);
+        print(waterPositionX + " " + waterPositionY);
+        Destroy(tileMap[waterPositionX, waterPositionY]);
+        Vector3 waterTilePosition = new Vector3(waterPositionX * 0.86f, waterPositionY * 0.86f, 0);
+        tileMap[waterPositionX, waterPositionY] = Instantiate(waterTile, waterTilePosition, Quaternion.identity);
     }
 
     // Update is called once per frame
