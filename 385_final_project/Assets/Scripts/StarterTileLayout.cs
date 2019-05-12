e324ab17a7d44cf962bb27e38cadd66406df3ea8
Petra Power <petra.powered@gmail.com>
2019-05-12 14:54:44 -0700

    Basic square map with just grass in 2D, includes simple textured water and plains tiles

---
+++ 385_final_project/Assets/Scripts/StarterTileLayout.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarterTileLayout : MonoBehaviour
+{
+    public int mapSize = 10;
+    public GameObject plainsTile;
+    public Camera gameCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start with plains
+        for(int i = 0; i < mapSize; i++)
+        { 
+            for(int j = 0; j < mapSize; j++)
+            {
+                Vector3 tilePosition = new Vector3(i * 0.86f, j * 0.86f, 0);
+                print(tilePosition);
+                Instantiate(plainsTile, tilePosition, Quaternion.identity);
+            }
+        }
+
+        // position the camera in the middle of the map
+        gameCamera.transform.position = new Vector3((mapSize/2) * 0.86f, (mapSize/2) * 0.86f, -20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
