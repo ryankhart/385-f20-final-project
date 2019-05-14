92fbd906563a1c983590f122390761dac643a180
Petra Power <petra.powered@gmail.com>
2019-05-14 00:20:34 -0700

    added tree generation

--- 385_final_project/Assets/Scripts/StarterTileLayout.cs
+++ 385_final_project/Assets/Scripts/StarterTileLayout.cs
@@ -20,34 +20,11 @@ public class StarterTileLayout : MonoBehaviour
         tileMap = new GameObject[mapSize, mapSize];
 
         GeneratePlains();
-        GenerateWater();
+        GenerateOtherTileGroups(waterTile, 0.86f); // water
         GenerateTrees();
 
         // position the camera in the middle of the map
         gameCamera.transform.position = new Vector3((mapSize / 2) * 0.86f, (mapSize / 2) * 0.86f, -10);
-    }
-
-    private void GenerateTrees()
-    {
-        System.Random rand = new System.Random((int)DateTime.Now.Ticks);
-
-        // find a plains tile
-        bool treeFound = false;
-        while(!treeFound)
-        {
-            int nextTreeX = rand.Next(0, mapSize);
-            int nextTreeY = rand.Next(0, mapSize);
-
-            if (tileMap[nextTreeX, nextTreeY].gameObject.tag.Equals("PlainsTile"))
-            {
-                float tilePositionX = tileMap[nextTreeX, nextTreeY].transform.position.x;
-                float tilePositionY = tileMap[nextTreeX, nextTreeY].transform.position.y;
-                Vector3 treePosition = new Vector3(tilePositionX, tilePositionY, 0);
-                Instantiate(tree, treePosition, Quaternion.Euler(-90,0,0));
-                print(tilePositionX + " : " + tilePositionY);
-                treeFound = true;
-            }
-        }
     }
 
     private void GeneratePlains()
@@ -62,26 +39,29 @@ public class StarterTileLayout : MonoBehaviour
         }
     }
 
-    private void GenerateWater()
+    // can be used for water, mountains, desert...
+    private void GenerateOtherTileGroups(GameObject tilePrefab, float offset)
     {
         // choose a random plains tile on the map
-        System.Random rand = new System.Random();
-        int waterIndexX = rand.Next(0,mapSize);
-        float waterPositionX = waterIndexX * 0.86f;
-        rand = new System.Random();
-        int waterIndexY = rand.Next(0,mapSize);
-        float waterPositionY = waterIndexY * 0.86f;
+        System.Random rand = new System.Random((int)DateTime.Now.Ticks);
+        int indexX = rand.Next(0,mapSize);
+        float tilePositionX = indexX * offset;
+        int indexY = rand.Next(0,mapSize);
+        float tilePositionY = indexY * offset;
 
         // destroy the plains tile
-        Destroy(tileMap[waterIndexX, waterIndexY]);
+        Destroy(tileMap[indexX, indexY]);
 
         // place new water tile - this will be the origin of the body of water
-        Vector3 waterTilePosition = new Vector3(waterPositionX, waterPositionY, 0);
-        tileMap[waterIndexX, waterIndexY] = Instantiate(waterTile, waterTilePosition, Quaternion.identity);
+        Vector3 waterTilePosition = new Vector3(tilePositionX, tilePositionY, 0);
+        tileMap[indexX, indexY] = Instantiate(waterTile, waterTilePosition, Quaternion.identity);
         print("Step 1");
 
-        int waterCount = (int) ((mapSize * mapSize) * 0.20); // up to 20 procent of the surface can be water
+        // up to 25 procent of the surface can be water, in practice it is always less
+        int waterCount = (int) ((mapSize * mapSize) * 0.25);
         int nextWater;
+
+        // randomly generate a group of water tiles
         for(int i = 0; i < waterCount; )
         {
             rand = new System.Random((int)DateTime.Now.Ticks);
@@ -90,38 +70,38 @@ public class StarterTileLayout : MonoBehaviour
             switch (nextWater)
             {
                 case 1:
-                    if (waterIndexX - 1 >= 0)  // check if we are past the map edge
+                    if (indexX - 1 >= 0)  // check if we are past the map edge
                     {
-                        waterIndexX -= 1;
-                        waterPositionX = waterIndexX * 0.86f;
-                        PlaceNewWaterTile(waterIndexX, waterPositionX, waterIndexY, waterPositionY);
+                        indexX -= 1;
+                        tilePositionX = indexX * offset;
+                        PlaceNewTile(tilePrefab, indexX, tilePositionX, indexY, tilePositionY);
                         i++;
                     }
                     break;
                 case 2:
-                    if (waterIndexX + 1 < mapSize)  // check if we are past the map edge
+                    if (indexX + 1 < mapSize)  // check if we are past the map edge
                     {
-                        waterIndexX += 1;
-                        waterPositionX = waterIndexX * 0.86f;
-                        PlaceNewWaterTile(waterIndexX, waterPositionX, waterIndexY, waterPositionY);
+                        indexX += 1;
+                        tilePositionX = indexX * offset;
+                        PlaceNewTile(tilePrefab, indexX, tilePositionX, indexY, tilePositionY);
                         i++;
                     }
                     break;
                 case 3:
-                    if (waterIndexY - 1 >= 0)  // check if we are past the map edge
+                    if (indexY - 1 >= 0)  // check if we are past the map edge
                     {
-                        waterIndexY -= 1;
-                        waterPositionY = waterIndexY * 0.86f;
-                        PlaceNewWaterTile(waterIndexX, waterPositionX, waterIndexY, waterPositionY);
+                        indexY -= 1;
+                        tilePositionY = indexY * offset;
+                        PlaceNewTile(tilePrefab, indexX, tilePositionX, indexY, tilePositionY);
                         i++;
                     }
                     break;
                 default:
-                    if (waterIndexY + 1 < mapSize)  // check if we are past the map edge
+                    if (indexY + 1 < mapSize)  // check if we are past the map edge
                     {
-                        waterIndexY += 1;
-                        waterPositionY = waterIndexY * 0.86f;
-                        PlaceNewWaterTile(waterIndexX, waterPositionX, waterIndexY, waterPositionY);
+                        indexY += 1;
+                        tilePositionY = indexY * offset;
+                        PlaceNewTile(tilePrefab, indexX, tilePositionX, indexY, tilePositionY);
                         i++;
                     }
                     break;
@@ -129,12 +109,38 @@ public class StarterTileLayout : MonoBehaviour
         }
     }
 
-    private void PlaceNewWaterTile(int waterX, float waterPositionX, int waterY, float waterPositionY)
+    private void PlaceNewTile(GameObject prefab, int indexX, float tilePositionX, int indexY, float tilePositionY)
     {
-        Vector3 waterTilePosition;
-        Destroy(tileMap[waterX, waterY]);
-        waterTilePosition = new Vector3(waterPositionX, waterPositionY, 0);
-        tileMap[waterX, waterY] = Instantiate(waterTile, waterTilePosition, Quaternion.identity);
+        Vector3 newTilePosition;
+        Destroy(tileMap[indexX, indexY]);
+        newTilePosition = new Vector3(tilePositionX, tilePositionY, 0);
+        tileMap[indexX, indexY] = Instantiate(prefab, newTilePosition, Quaternion.identity);
+    }
+
+    private void GenerateTrees()
+    {
+        System.Random rand = new System.Random((int)DateTime.Now.Ticks);
+        // seed position for a tree
+        int nextTreeX;
+        int nextTreeY;
+
+        // find a plains tile
+        int treeCount = (int) (mapSize * mapSize * 0.3); // ~30% of map will be trees
+        while (treeCount > 0)
+        {
+            nextTreeX = rand.Next(0, mapSize);
+            nextTreeY = rand.Next(0, mapSize);
+
+            if (tileMap[nextTreeX, nextTreeY].gameObject.tag.Equals("PlainsTile"))
+            {
+                float tilePositionX = tileMap[nextTreeX, nextTreeY].transform.position.x;
+                float tilePositionY = tileMap[nextTreeX, nextTreeY].transform.position.y;
+                Vector3 treePosition = new Vector3(tilePositionX + 0.43f, tilePositionY + 0.43f, 0);
+                Instantiate(tree, treePosition, Quaternion.Euler(-90, 0, 0)); // rotatet to top down view
+                print(tilePositionX + " : " + tilePositionY);
+                treeCount--;
+            }
+        }
     }
 
     // Update is called once per frame
