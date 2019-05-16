6fc6ce0dba0d6e88a6ccbfd629b81c286b5492eb
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-16 00:58:48 -0700

    Add script for basic toggle for Scry overlay

---
+++ 385_final_project/Assets/Scripts/ScryMode.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScryMode : MonoBehaviour
+{
+    public GameObject m_ScryOverlay;
+    public Button m_ScryToggleButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ScryToggleButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Toggle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Toggle()
+    {
+        if (IsActive()) Deactivate();
+        else Activate();
+    }
+
+    bool IsActive()
+    {
+        return (m_ScryOverlay.activeSelf);
+    }
+
+    void Activate()
+    {
+        m_ScryOverlay.SetActive(true);
+    }
+
+    void Deactivate()
+    {
+        m_ScryOverlay.SetActive(false);
+    }
+}
