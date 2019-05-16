949c6f13df60d6eda41cc9e1b9ac6338780311f7
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-16 02:26:49 -0700

    Add FastMode for CameraController

--- 385_final_project/Assets/Scripts/CameraController.cs
+++ 385_final_project/Assets/Scripts/CameraController.cs
@@ -1,10 +1,13 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
 
     public float m_panSpeed = 2f;
     public float m_zoomSpeed = 0.5f;
+    public float m_fastModeMultiplier = 2;
+
+    private bool m_isInFastMode = false;
 
     // Update is called once per frame
     void Update()
@@ -36,5 +39,25 @@ public class CameraController : MonoBehaviour
 
         // Update camera position
         transform.position = position;
+    }
+
+    public void EnableFastMode()
+    {
+        if (!m_isInFastMode)
+        {
+            print("Enabling Camera Fast Mode");
+            m_isInFastMode = true;
+            m_panSpeed *= m_fastModeMultiplier;
+        }
+    }
+
+    public void DisableFastMode()
+    {
+        if (m_isInFastMode)
+        {
+            print("Disabling Camera Fast Mode");
+            m_isInFastMode = false;
+            m_panSpeed /= m_fastModeMultiplier;
+        }
     }
 }
