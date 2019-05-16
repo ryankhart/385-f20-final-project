3509c8f23b1e5abfc653e5055730e7fe06a58de9
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-16 01:42:45 -0700

    Enable CameraController if in ScryMode, disable if not

--- 385_final_project/Assets/Scripts/ScryMode.cs
+++ 385_final_project/Assets/Scripts/ScryMode.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,12 +7,17 @@ public class ScryMode : MonoBehaviour
 {
     public GameObject m_Overlay;
     public Button m_ToggleButton;
+    public Camera m_Camera;
+
+    private CameraController m_CameraController;
 
     // Start is called before the first frame update
     void Start()
     {
         // If the the toggle button is clicked, run TaskOnClick().
         m_ToggleButton.onClick.AddListener(TaskOnClick);
+
+        m_CameraController = m_Camera.GetComponent<CameraController>();
     }
 
     void TaskOnClick()
@@ -40,10 +45,12 @@ public class ScryMode : MonoBehaviour
     void Activate()
     {
         m_Overlay.SetActive(true);
+        m_CameraController.enabled = true;
     }
 
     void Deactivate()
     {
         m_Overlay.SetActive(false);
+        m_CameraController.enabled = false;
     }
 }
