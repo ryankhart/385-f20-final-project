50e910815e9e29f381abbd7088e8ee307269225a
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-21 00:24:59 -0700

    Made variable names consistent

--- 385_final_project/Assets/Scripts/Alert.cs
+++ 385_final_project/Assets/Scripts/Alert.cs
@@ -7,12 +7,13 @@ public class Alert : MonoBehaviour
     public GameObject m_target;
     
     private Vector3 m_difference;
+    private float m_rotationZ;
 
     // Update is called once per frame
     void Update()
     {
         m_difference = m_target.transform.position - m_target.transform.position;
-        float rotationZ = Mathf.Atan2(m_difference.y, m_difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        m_rotationZ = Mathf.Atan2(m_difference.y, m_difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, m_rotationZ);
     }
 }
