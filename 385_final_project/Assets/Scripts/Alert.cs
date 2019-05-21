b583e4d827f54289fb19e1a6d6eb1f6a555062c4
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-21 00:15:06 -0700

    My attempt at rotating the AlertObject's Z rotation toward object
    It doesn't seem to work in my test scene though.

---
+++ 385_final_project/Assets/Scripts/Alert.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alert : MonoBehaviour
+{
+    public GameObject m_target;
+    
+    private Vector3 m_difference;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_difference = m_target.transform.position - m_target.transform.position;
+        float rotationZ = Mathf.Atan2(m_difference.y, m_difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+    }
+}
