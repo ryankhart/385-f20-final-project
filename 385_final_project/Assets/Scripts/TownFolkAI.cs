9023be933e904d576bb5cd3dfadbdfa46ba55143
Petra Power <petra.powered@gmail.com>
2019-05-21 14:19:56 -0700

    villager spawner in progress, posting changes to TownFolkAI script

--- 385_final_project/Assets/Scripts/TownFolkAI.cs
+++ 385_final_project/Assets/Scripts/TownFolkAI.cs
@@ -187,7 +187,6 @@ public class TownFolkAI : MonoBehaviour
     private void FindPath()
     {
         Transform startPosition = transform;
-        Transform endPosition = FindTarget();
 
         if (endPosition != null)
         {
