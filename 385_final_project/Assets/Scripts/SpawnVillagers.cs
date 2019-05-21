2d9a03196b877c31eed264825d97b178b37084bf
Petra Power <petra.powered@gmail.com>
2019-05-21 14:19:56 -0700

    villager spawner in progress, posting changes to TownFolkAI script

---
+++ 385_final_project/Assets/Scripts/SpawnVillagers.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnVillagers : MonoBehaviour
+{
+    public GameObject lumberjack;
+
+    private int mapSize;
+    private int currentNumHouses = 0;
+    private List<GameObject> villagers;
+    private StarterTileLayout mapStarterScript;
+
+    private float tileOffset = 0.86f;
+    private float centerOffset = 0.43f;
+
+    private void Start()
+    {
+        // get map size
+        GameObject mapStarter = GameObject.Find("TileLayoutStarter");
+        mapStarterScript = mapStarter.GetComponent<StarterTileLayout>();
+        GameObject gridCreator = GameObject.Find("GridCreator(Clone)");
+        mapSize = gridCreator.GetComponent<GridManager>().numberOfColumns;
+
+        // limit max num of villagers to a number based on map size
+        // TODO: come up with a better number selection
+        villagers = new List<GameObject>(mapSize);
+    }
+
+    private void Update()
+    {
+        // one villager per house
+        // TODO: come up with a better algorithm for ratio between houses and villagers
+        currentNumHouses = GameObject.FindGameObjectsWithTag("Home").Length;
+        if(villagers.Count < currentNumHouses && villagers.Count <= mapSize)
+        {
+            bool foundSpot = false;
+            // find an empty plains tile
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    string tileTag = mapStarterScript.getTileTag(i,j);
+                    if (tileTag.Equals("PlainsTile"))
+                    {
+                        //float lumberjackY = (float) (lumberjack.GetComponent<Head>().GetComponent<Renderer>().bounds.size.y + 0.5);
+                        GameObject jack = Instantiate(lumberjack, new Vector3(i * tileOffset + centerOffset, 0.439f, j * tileOffset + centerOffset), Quaternion.identity);
+                        villagers.Add(jack);
+                        // TODO: ask Jonathan to add method to stop a position and not destroy the prefab
+                        jack.GetComponent<TownFolkAI>().resourceTag = "Home";
+                        foundSpot = true;
+                        break;
+                    }
+                }
+                if(foundSpot)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
--- 385_final_project/Assets/Scripts/TownFolkAI.cs
+++ 385_final_project/Assets/Scripts/TownFolkAI.cs
@@ -19,6 +19,11 @@ public class TownFolkAI : MonoBehaviour
     private Vector3 targetPoint;
     private Vector3 direction;
     private Quaternion targetRotation;
+    private RaycastHit avoidanceHit;
+    private Vector3 hitNormal;
+
+    //private Transform startPosition;
+    //private Transform endPosition;
 
     public Node startNode { get; set; }
     public Node goalNode { get; set; }
@@ -181,7 +186,7 @@ public class TownFolkAI : MonoBehaviour
     //A* finds the path that the TownFolk should take.
     private void FindPath()
     {
-        Transform startPosition = transform;
+        Transform startPosition = startCube.transform;
         Transform endPosition = FindTarget();
 
         if (endPosition != null)
