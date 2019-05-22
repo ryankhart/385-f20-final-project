ee23b5e41a97a146b2224bce9ab2ff4c48e3d36a
Petra Power <petra.powered@gmail.com>
2019-05-22 14:57:26 -0700

    added script to village center

--- 385_final_project/Assets/Scripts/TownFolkAI.cs
+++ 385_final_project/Assets/Scripts/TownFolkAI.cs
@@ -67,7 +67,8 @@ public class TownFolkAI : MonoBehaviour
         FindNode();
 
         // Checks if target is too close
-        if (Vector3.Distance(targetPoint, transform.position) < toleranceRadius)
+        // WARNING, YOU ARE FORGETTING THE VILLAGER HAS Y POSITION 0.439
+        if (Vector3.Distance(targetPoint, new Vector3(transform.position.x, 0, transform.position.z)) < toleranceRadius)
         {
             //If target is too close that means we need to peform an action!
 
@@ -112,6 +113,7 @@ public class TownFolkAI : MonoBehaviour
         direction.y = 0;
 
         // look
+        // if statement prevent turning when at target and that stupid zero vector warning
         if (Vector3.Distance(direction, Vector3.zero) > 0.01)
         {
             targetRotation = Quaternion.LookRotation(direction);
@@ -225,16 +227,15 @@ public class TownFolkAI : MonoBehaviour
 
             targetObject.GetComponent<ResourceCounter>().numberOfResources -= 1;
             inventory += 1;
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> added script to village center
 
 
 
 
+            if(inventory == 5)
+            {
+                resourceTag = "VillageCenter";
+                return;
+            }
         }
     }
 
