dbeec6557946280509d34d9e70a3043bd4a2ee42
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-23 00:44:41 -0700

    Alert is now correctly rotated to point towards target
    It disregards the y axis and only calculates based on X and Z.

--- 385_final_project/Assets/Scripts/Alert.cs
+++ 385_final_project/Assets/Scripts/Alert.cs
@@ -8,15 +8,20 @@ public class Alert : MonoBehaviour
     public GameObject m_target;
     
     private Vector3 m_difference;
-    private float m_rotationZ;
+    private float m_radiansToRotateZ;
+    private float m_degreesToRotateZ;
 
     // Update is called once per frame
     void Update()
     {
         m_difference = m_player.transform.position - m_target.transform.position;
-        print("Difference: X=" + m_difference.x + "  Y=" + m_difference.y + "  Z=" + m_difference.z);
-        m_rotationZ = Mathf.Atan2(m_difference.z, m_difference.x) * Mathf.Rad2Deg;
-        print("Z rotation is " + m_rotationZ);
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, m_rotationZ);
+
+        // SOH CAH TOA
+        // Arctan( opposite (y) over adjacent (x) ) = radian angle
+        m_radiansToRotateZ = Mathf.Atan2(m_difference.z, m_difference.x);
+        m_degreesToRotateZ = m_radiansToRotateZ * Mathf.Rad2Deg;
+
+        // Not sure why, but without the "+ 90" here, it's always 90 degrees off.
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, m_degreesToRotateZ + 90);
     }
 }
