1fff9d1f11ff3eaf9ab489616f41628551a8476c
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-22 20:30:05 -0700

    Enable player visibility only after being placed on map
    This way, we can keep the player character renderer disabled (invisible) until placed on map.

--- 385_final_project/Assets/Scripts/PlayerController.cs
+++ 385_final_project/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Renderer m_renderer;
+
     // Start is called before the first frame update
     void Start()
     {
         // TODO: Come back later and replace these static coordinates with a
         // raycast (?) of where the camera is pointed at at the start of the game.
         transform.position = new Vector3(10.75f, 0f, 10.75f);
+        m_renderer = GetComponent<Renderer>();
+        m_renderer.enabled = true;
     }
 
     // Update is called once per frame
