36ed9134e880fc765e6b465e000c96463c01c7ec
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-22 20:19:34 -0700

    Added script to place dummy player character on map
    Coordinates are static for now, but will fix later.

---
+++ 385_final_project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TODO: Come back later and replace these static coordinates with a
+        // raycast (?) of where the camera is pointed at at the start of the game.
+        transform.position = new Vector3(10.75f, 0f, 10.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
