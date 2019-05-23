729e829fceae4eb7d666b74abb3bcb95234bd672
Ryan Hart <3673962+ryankhart@users.noreply.github.com>
2019-05-22 23:38:19 -0700

    Added console print statements and made it so that only z and x matter

--- 385_final_project/Assets/Scripts/Alert.cs
+++ 385_final_project/Assets/Scripts/Alert.cs
@@ -13,8 +13,10 @@ public class Alert : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        m_difference = m_target.transform.position - m_target.transform.position;
-        m_rotationZ = Mathf.Atan2(m_difference.y, m_difference.x) * Mathf.Rad2Deg;
+        m_difference = m_player.transform.position - m_target.transform.position;
+        print("Difference: X=" + m_difference.x + "  Y=" + m_difference.y + "  Z=" + m_difference.z);
+        m_rotationZ = Mathf.Atan2(m_difference.z, m_difference.x) * Mathf.Rad2Deg;
+        print("Z rotation is " + m_rotationZ);
         transform.rotation = Quaternion.Euler(0.0f, 0.0f, m_rotationZ);
     }
 }
