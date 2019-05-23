cb24df88a0f639ea2fd3b3e2f263e04f9adc1059
Petra Power <petra.powered@gmail.com>
2019-05-22 19:14:46 -0700

    villager brings collected resources to village center and goes on to next resource

--- 385_final_project/Assets/Scripts/TrackStorageResources.cs
+++ 385_final_project/Assets/Scripts/TrackStorageResources.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TrackStorageResources : MonoBehaviour
 {
-    private Dictionary<string, int> resources;
+    private Dictionary<string, int> resources = new Dictionary<string, int>();
 
     // Start is called before the first frame update
     void Start()
