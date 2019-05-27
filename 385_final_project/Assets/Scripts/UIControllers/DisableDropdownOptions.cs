a5fef1e08925a4d500c8ad81a51aad75779c9708
Petra Power <petra.powered@gmail.com>
2019-05-26 17:54:39 -0700

    Big commit 1) centered camera to approx center of the map + put sparkles where player will apppear on an empty PlainsTile 2) disabled all buildings besides village center at the beginning of the game

---
+++ 385_final_project/Assets/Scripts/UIControllers/DisableDropdownOptions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableDropdownOptions : MonoBehaviour
+{
+    private Dropdown dropdown;
+    private List<Dropdown.OptionData> smallList;
+    private List<Dropdown.OptionData> completeList;
+    private bool displayFullList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smallList = new List<Dropdown.OptionData>();
+        completeList = new List<Dropdown.OptionData>();
+
+        dropdown = GetComponent<Dropdown>();
+
+        smallList.Add(dropdown.options[0]);
+        smallList.Add(dropdown.options[1]);
+
+        completeList.AddRange(smallList);
+        completeList.Add(dropdown.options[2]);
+        completeList.Add(dropdown.options[3]);
+        completeList.Add(dropdown.options[4]);
+
+        dropdown.ClearOptions();
+        dropdown.AddOptions(smallList);
+
+        displayFullList = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!displayFullList && GameObject.FindWithTag("VillageCenter"))
+        {
+            dropdown.ClearOptions();
+            dropdown.AddOptions(completeList);
+        }
+    }
+}
