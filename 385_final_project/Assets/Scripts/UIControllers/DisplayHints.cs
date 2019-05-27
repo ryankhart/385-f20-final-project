7b98d07d937283a984085b96ddf911574b93a73b
Petra Power <petra.powered@gmail.com>
2019-05-26 17:54:39 -0700

    Big commit 1) centered camera to approx center of the map + put sparkles where player will apppear on an empty PlainsTile 2) disabled all buildings besides village center at the beginning of the game

---
+++ 385_final_project/Assets/Scripts/UIControllers/DisplayHints.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayHints : MonoBehaviour
+{
+    private Dictionary<string, Transform> hints;
+
+    void Start()
+    {
+        hints = new Dictionary<string, Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<CanvasGroup>())
+            {
+                hints.Add(transform.GetChild(i).name, transform.GetChild(i));
+            }
+        }
+        StartCoroutine(WaitToDisplay());
+    }
+
+    public IEnumerator WaitToDisplay()
+    {
+        yield return new WaitForSeconds(10);
+        CanvasGroup group = null;
+        foreach (KeyValuePair<string, Transform> item in hints)
+        {
+            print(item.Key);
+            if (item.Key == "BuildingHint")
+            {
+                group = item.Value.GetComponent<CanvasGroup>();
+            }
+        }
+        if (group != null)
+        {
+            while (group.alpha < 1)
+            {
+                group.alpha += Time.deltaTime / 2;
+                yield return null;
+            }
+        }
+
+        yield return null;
+    }
+}
