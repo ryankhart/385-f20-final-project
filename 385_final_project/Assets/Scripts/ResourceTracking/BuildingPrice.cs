f18360baae32b85c0b0dcfdc4564f0c0ce700054
Petra Power <petra.powered@gmail.com>
2019-05-27 18:29:58 -0700

    added basic price system to house, village center, and tavern - first village center is for free, second is paid like all following buildings

---
+++ 385_final_project/Assets/Scripts/ResourceTracking/BuildingPrice.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPrice : MonoBehaviour
+{
+    private Dictionary<string, int> price;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // can't use tags because upon initiation, building have MovingBuilding tag
+        // TODO resolve
+        price = new Dictionary<string, int>();
+        if(gameObject.name.Contains("House"))
+        {
+            price.Add("Tree",5);
+            price.Add("Stone", 0);
+            price.Add("Copper", 0);
+        }
+        else if (gameObject.name.Contains("Tavern"))
+        {
+            price.Add("Tree", 20);
+            price.Add("Stone", 10);
+            price.Add("Copper", 0);
+        }
+        else if (gameObject.name.Contains("VillageCenter"))
+        {
+            price.Add("Tree", 50);
+            price.Add("Stone", 20);
+            price.Add("Copper", 10);
+        }
+    }
+
+    public Dictionary<string, int> GetPrice()
+    {
+        return price;
+    }
+
+    // TODO method for canAfford?
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
