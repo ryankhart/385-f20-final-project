41f103bd7b88eb443d30df7a4cd6b5be4f0bcd9d
Petra Power <petra.powered@gmail.com>
2019-05-30 13:59:01 -0700

    camera can't move past a ceiling - still buggy, though, it keeps shaking

---
+++ 385_final_project/Assets/UpdateVillagerList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateVillagerList : MonoBehaviour
+{
+    private Transform header;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        header = gameObject.transform.Find("Text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddVillager(int index)
+    {
+        Text newVillInfo = gameObject.AddComponent<Text>();
+        //trans.anchoredPosition = new Vector2(x, y);
+    }
+
+    // TODO: public void RemoveVillager()
+}
